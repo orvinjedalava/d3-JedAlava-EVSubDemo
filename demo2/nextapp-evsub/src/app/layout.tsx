4f0e9719f45b6e74c37a16277b7d3682c7c5d74a
orvinjedalava <ojedbub@gmail.com>
2025-09-02 17:32:16 +1000

    Commit

--- demo2/nextapp-evsub/src/app/layout.tsx
+++ demo2/nextapp-evsub/src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
                 title="heroui.com homepage"
               >
                 <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
+                <p className="text-primary">AI</p>
               </Link>
             </footer>
           </div>
