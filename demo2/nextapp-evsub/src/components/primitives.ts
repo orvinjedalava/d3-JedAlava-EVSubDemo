fc326f1efaf03cee3aed4c1abe4932ccc61ec8c9
orvinjedalava <ojedbub@gmail.com>
2025-09-02 18:38:27 +1000

    commit

--- demo2/nextapp-evsub/src/components/primitives.ts
+++ demo2/nextapp-evsub/src/components/primitives.ts
@@ -4,7 +4,7 @@ export const title = tv({
   base: "tracking-tight inline font-semibold",
   variants: {
     color: {
-      violet: "from-[#FF1CF7] to-[#b249f8]",
+      violet: "from-[#FF1CF7] to-[#8c39f5]",
       yellow: "from-[#FF705B] to-[#FFB457]",
       blue: "from-[#5EA2EF] to-[#0072F5]",
       cyan: "from-[#00b7fa] to-[#01cfea]",
