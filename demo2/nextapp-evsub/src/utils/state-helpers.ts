0d12e2176930c758eb6e6f934f76dd8f0314f8f3
orvinjedalava <ojedbub@gmail.com>
2025-09-09 19:28:27 +1000

    yay

--- demo2/nextapp-evsub/src/utils/state-helpers.ts
+++ demo2/nextapp-evsub/src/utils/state-helpers.ts
@@ -10,28 +10,6 @@ import type {
 
 import { CardDisplayMode } from '@/types';
 
-export const getDisplayCoordinates = (carInfos: CarInfo[]): CarState[] => {
-  const carsCoordinates =  [
-    { width: 253, height: 350, top: 80, left: 50, opacity: 1 , zIndex: 0},
-    { width: 253, height: 350, top: 350, left: 500, opacity: 1 , zIndex: 0},
-    // { width: 253, height: 276, top: 0, left: 250, opacity: 1 },
-    // { width: 253, height: 276, top: 0, left: 350, opacity: 1 },
-  ]
-
-  return carInfos.map((info, index) => {
-    // Get the display properties for this card, or use defaults if not available
-    const displayProperties = carsCoordinates[index];
-
-    // Return the CarState object with separate info and displayProperties
-    return {
-      displayProperties,
-      info,
-      displayMode: CardDisplayMode.ShowCriteria
-    };
-  });
-
-};
-
 export const generateCarsStateFrom = (carGroupInfos: CarGroupInfo[]): CarGroupState[] => {
   const carsCoordinates =  [
     { width: 253, height: 350, top: 80, left: 50, opacity: 1 , zIndex: 0},
