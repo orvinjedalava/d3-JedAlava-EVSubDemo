8811ca5f5181edd6882da18fdaf6b8775c1fe2a2
orvinjedalava <ojedbub@gmail.com>
2025-09-12 12:18:53 +1000

    test

--- demo3/nextapp-evsub/src/components/car-park.tsx
+++ demo3/nextapp-evsub/src/components/car-park.tsx
@@ -1,8 +1,19 @@
+import { PlusIcon } from "@/components/icons";
+import { Button } from "@heroui/button";
+import {  PlusCircleIcon } from "@heroicons/react/24/outline";
+
 export const CarPark = () => {
   return (
     <div
       // className={`absolute top-[800px] left-[100px] min-w-[${1536 * 0.9}px] max-w-[${1536 * 0.9}px] bg-black/80 dark:bg-white/80 border`}
-      className={`absolute top-[910px] left-[120px] min-w-[1300px] max-w-[1300px] min-h-[130px] rounded-tl-lg rounded-tr-lg bg-black/40 dark:bg-white/40`}
-    ></div>
+      className={`absolute top-[910px] left-[120px] min-w-[1300px] max-w-[1300px] min-h-[130px] 
+        flex flex-col items-center justify-center
+        rounded-tl-lg rounded-tr-lg bg-black/40 dark:bg-white/40`}
+    >
+      <Button isIconOnly variant="light">
+        <PlusCircleIcon className="text-secondary size-8"  />
+      </Button>
+      <span>Favourited vehicles will appear here OR drag vehicles that interest you</span>
+    </div>
   );
-}
+};
