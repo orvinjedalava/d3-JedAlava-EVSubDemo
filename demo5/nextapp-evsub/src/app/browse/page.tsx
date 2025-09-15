78f8c33f3605c3185f699e873a016d49f163498b
orvinjedalava <ojedbub@gmail.com>
2025-09-15 20:36:30 +1000

    test

--- demo5/nextapp-evsub/src/app/browse/page.tsx
+++ demo5/nextapp-evsub/src/app/browse/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ChatUI } from "@/components/chat-ui";
 import { motion } from 'framer-motion';
 import { CardsPanel } from '@/components/cards-panel';
 import { useTransitionState } from 'next-transition-router';
@@ -15,21 +14,6 @@ export default function BrowsePage() {
   
   return (
     <section className="flex-grow w-full relative flex flex-col gap-2 lg:flex-row">
-      {/* <div className="h-screen lg:h-auto lg:w-1/4 flex flex-col py-8">
-      <motion.div
-        className="flex-grow flex flex-col"
-        initial="initial"
-        animate={ stage === 'leaving' ? motionOpacity.exit : motionOpacity.animate }
-        variants={motionOpacity}
-        transition={{ duration: appSettings.transitionDuration }}
-      >
-      <ChatUI initialMessages={getInitialMessages()} height="800px" />
-      
-    </motion.div></div>
-      <div className="h-screen lg:h-auto lg:w-3/4 flex flex-col py-8 justify-center">
-        <CardsPanel/>
-      </div> */}
-
       <CardsPanel/>
     </section>
   );
